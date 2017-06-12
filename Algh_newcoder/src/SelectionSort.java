04f1ad01601a55f0310e702bf3cea5fa45dadb1a
annelliott <1842714077@qq.com>
2017-06-12 10:20:33 +0800

    include all the algorithm

--- Algh_newcoder/src/SelectionSort.java
+++ Algh_newcoder/src/SelectionSort.java
@@ -36,7 +36,7 @@ public class SelectionSort {
 		Node node1 = new Node(2);
 		Node node2 = new Node(1);
 		Node node3 = new Node(7);
-		Node node4 = new Node(1);
+		Node node4 = new Node(4);
 		node1.next = node2;
 		node2.next = node3;
 		node3.next = node4;
